2e057229ec68d2cc05facbfbd5f09ba3398c8b8f
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-05-30 13:41:20 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// Importing productNames from names.json file
+const productNames = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/names.json`)
+);
+
+//Middlewares
+app.use(express.json())
+
+// GET endpoint for sending the products to client by id
+//Endpoint - /api/v1/names/:id
+
+
+module.exports = app;
